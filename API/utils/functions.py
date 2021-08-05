89ded03f45f50abdddd575b22449b06a956885a3
zc <zhangchanget@126.com>
2021-08-05 11:58:09 +0800

    2021.8.5号飞书自动化项目实战(接口自动化)

--- API/utils/functions.py
+++ API/utils/functions.py
@@ -3,8 +3,19 @@
 # @Time    : 2021/08/05 10:40
 # @Author  : zc
 # @File    : functions.py
+import os
 
 
-# class Functions:
-#
-#     def upPath(self):
+class Functions:
+
+    def upPath(self):
+
+        basePath = os.path.dirname(os.path.dirname(__file__))
+
+
+
+
+
+
+if __name__ == '__main__':
+    Functions().upPath()
